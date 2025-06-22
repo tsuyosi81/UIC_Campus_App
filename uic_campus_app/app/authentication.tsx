e7434daebf8b2fb46d06a3d793614153bf886f77
Justin Laptop <justin.hompho@gmail.com>
2025-06-22 16:15:58 -0500

    added new pages and buttons

---
+++ uic_campus_app/app/authentication.tsx
@@ -0,0 +1,20 @@
+
+import { Text, View } from "react-native";
+
+export default function Authentication() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is the authentication.tsx
+      </Text>
+      <Text>
+        made by justin
+      </Text>
+    </View>
+  );
+}
--- uic_campus_app/app/index.tsx
+++ uic_campus_app/app/index.tsx
@@ -11,12 +11,32 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>This is the first start of the project!
+      <Text>This is a merge conflict test!
       </Text>
       <Text>
         run "npx expo start" to run a developement display
       </Text>
-      <Button onPress={() => router.navigate('/create_posts')}>Go to About</Button>;
+      <Button
+      title="Go to Create Post"
+      onPress={() => router.push("/create_posts")}
+      />
+      <Button
+      title="Go to Authentication"
+      onPress={() => router.push("/authentication")}
+      />
+      <Button
+      title="Go to Community"
+      onPress={() => router.push("/community")}
+      />
+      <Button
+      title="Go to DM"
+      onPress={() => router.push("/dm")}
+      />
+      <Button
+      title="Go to Events"
+      onPress={() => router.push("/event")}
+      />
+      
     </View>
   );
 }
