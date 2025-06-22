426d0e758ff1c185cd0e6a0f0c6264a1ba5d0d11
Tsuyoshi <tharayama81@gmail.com>
2025-06-22 16:15:37 -0500

    Added pages for each feature

--- uic_campus_app/app/index.tsx
+++ uic_campus_app/app/index.tsx
@@ -11,12 +11,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>This is the first start of the project!
-      </Text>
-      <Text>
-        run "npx expo start" to run a developement display
-      </Text>
-      <Button onPress={() => router.navigate('/create_posts')}>Go to About</Button>;
+      
+      <Text>Naviate using the following buttons.</Text>
+      
+      <Text>run "npx expo start" to run a developement display</Text>
+      
+      <Button title="Go to Create Posts" onPress={() => router.push('/create_posts')} />
+      <Button title="Go to Loading" onPress={() => router.push('/loading')} />
+      <Button title="Go to Notifications" onPress={() => router.push('/notifications')} />
+      <Button title="Go to Profile Setup" onPress={() => router.push('/profile_setup')} />
+      <Button title="Go to Safety Map" onPress={() => router.push('/safety_map')} />
+      <Button title="Go to Search" onPress={() => router.push('/search')} />
+      <Button title="Go to Social Feed" onPress={() => router.push('/social_feed')} />
+      <Button title="Go to Test" onPress={() => router.push('/test')} />
+      <Button title="Go to User Profile" onPress={() => router.push('/user_profile')} />
     </View>
   );
 }
---
+++ uic_campus_app/app/test.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Test() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is the test.tsx
+      </Text>
+    </View>
+  );
+}
