b5fa6d0e56f236404fd9d90acf30bd3f560965aa
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-07-09 14:37:53 -0500

    Started work on adding tabs

--- uic_campus_app/app/components/notification_style.js
+++ uic_campus_app/app/components/notification_style.js
@@ -5,6 +5,28 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#ffffff"
     },
+    tabContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: 20,
+        paddingBottom: 15,
+        gap: 125,
+        borderBottomWidth: 1,
+        borderBottomColor: "#D1D8DD",
+    },
+    tabText: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "#56636F"
+    },
+    activeTabText: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "#000000",
+        borderBottomWidth: 3,
+        borderBottomColor: "#56636F",
+    }
 })
 
 export default styles
--- uic_campus_app/app/notifications.tsx
+++ uic_campus_app/app/notifications.tsx
@@ -1,10 +1,22 @@
-import { Text, View } from "react-native";
-import styles from "./components/notification_style.js"
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import styles from "./components/notification_style.js";
 export default function Notifications() {
+
+  const [activeTab, setActiveTab] = React.useState(0)
+
   return (
     <>
       <View style={styles.container}>
-      </View>
+        <View style={styles.tabContainer}>
+          <TouchableOpacity>
+            <Text style={styles.tabText}>All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.activeTabText}>Mentions</Text>
+          </TouchableOpacity>
+        </View>          
+      </View>      
     </>
   );
 }
