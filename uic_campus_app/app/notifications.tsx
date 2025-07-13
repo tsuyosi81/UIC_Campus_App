99cf2d8a1245fe3f6bc70b0211d6a70e375ca6af
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-07-13 18:20:59 -0500

    Added other types of notifications and condensed into components

--- uic_campus_app/app/notifications.tsx
+++ uic_campus_app/app/notifications.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Link } from "expo-router";
-import { Text, TouchableOpacity, View, Image } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import Event from "./components/events.tsx";
+import Mention from "./components/mention.tsx";
 import styles from "./components/notification_style.js";
+import Reminder from "./components/reminder.tsx";
+import Warning from "./components/warning.tsx";
+
 export default function Notifications() {
 
   const [activeTab, setActiveTab] = React.useState('All')
@@ -17,127 +21,68 @@ export default function Notifications() {
             <Text style={[styles.tabText, activeTab === 'Mentions' && styles.activeTabText]}>Mentions</Text>
           </TouchableOpacity>
         </View>
-          <View style={styles.notificationContainer}>
-            {(activeTab === 'Mentions' || activeTab === 'All') && <Link href="/social_feed" asChild>
-              <TouchableOpacity>
-                <View style={styles.mentionNotification}>
-                  <View style={styles.mentionTop}>
-                    <Image
-                      style={{
-                        width: 25,
-                        height: 25
-                      }}
-                      source={require("./images/like.png")}
-                    />
-                    <Image
-                      style={{
-                        width: 30,
-                        height: 30
-                      }}
-                      source={require("./images/avatar.png")}
-                    />
-                  </View>
-                  <View style={styles.mentionText}>
-                    <Text><Text style={styles.grayed}>@userID</Text> <Text style={styles.bold}>reacted</Text> to your post.</Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            </Link>}
-            {(activeTab === 'Alerts' || activeTab === 'All') && <Link href="/safety_map" asChild>
-            <TouchableOpacity>
-              <View style={styles.alertNotification}>
-                <Image
-                  style={{
-                    width: 25,
-                    height: 21
-                  }}
-                  source={require("./images/warn1.png")}
-                />
-                <View>
-                  <Text style={{marginRight: 10}}>
-                    <Text style={styles.bold}>UIC Alert:</Text> <Text style={styles.grayed}>[alertDetails]</Text>. Avoid <Text style={styles.grayed}>[location]</Text>. Tap for more.
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </Link>}
-          {(activeTab === 'Mentions' || activeTab === 'All') && <Link href="/social_feed" asChild>
-            <TouchableOpacity>
-              <View style={styles.mentionNotification}>
-                <View style={styles.mentionTop}>
-                  <Image
-                    style={{
-                      width: 25,
-                      height: 25
-                    }}
-                    source={require("./images/comment.png")}
-                  />
-                  <Image
-                    style={{
-                      width: 30,
-                      height: 30
-                    }}
-                    source={require("./images/avatar.png")}
-                  />
-                </View>
-                <View style={styles.mentionText}>
-                  <Text><Text style={styles.grayed}>@userID</Text> <Text style={styles.bold}>commented</Text> on your post.</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </Link>}
-          {(activeTab === 'Mentions' || activeTab === 'All') && <Link href="/social_feed" asChild>
-            <TouchableOpacity>
-              <View style={styles.mentionNotification}>
-                <View style={styles.mentionTop}>
-                  <Image
-                    style={{
-                      width: 25,
-                      height: 25
-                    }}
-                    source={require("./images/heart.png")}
-                  />
-                  <Image
-                    style={{
-                      width: 30,
-                      height: 30
-                    }}
-                    source={require("./images/avatar.png")}
-                  />
-                  <Image
-                    style={{
-                      width: 30,
-                      height: 30
-                    }}
-                    source={require("./images/avatar.png")}
-                  />
-                </View>
-                <View style={styles.mentionText}>
-                  <Text><Text style={styles.grayed}>@userID1</Text> and <Text style={styles.grayed}>@userID2</Text> <Text style={styles.bold}>reacted</Text> to your post.</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </Link>}
-          {(activeTab === 'Alerts' || activeTab === 'All') && <Link href="/safety_map" asChild>
-            <TouchableOpacity>
-              <View style={styles.alertNotification}>
-                <Image
-                  style={{
-                    width: 25,
-                    height: 25
-                  }}
-                  source={require("./images/warn2.png")}
-                />
-                <View>
-                  <Text style={{marginRight: 10}}>
-                    <Text style={styles.bold}>Crowd warning:</Text> <Text style={styles.grayed}>@userID</Text> reported unsafe conditions at <Text style={styles.grayed}>[location]</Text>. Tap to confirm.
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </Link>}
-        </View>               
+          <ScrollView style={styles.notificationContainer}>
+            {(activeTab === 'Mentions' || activeTab === 'All') && <Mention 
+              link="/social_feed" 
+              avatarSource={require("./images/avatar.png")} 
+              actionSource={require("./images/like.png")} 
+              users="Bob" 
+              action="reacted"
+            />}
+            {(activeTab === 'Alerts' || activeTab === 'All') && <Warning
+              link="/safety_map"
+              action="alert"
+              location="[location]"
+              alertDetails="[alertDetails]."
+            />}
+          {(activeTab === 'Mentions' || activeTab === 'All') && <Mention 
+              link="/social_feed" 
+              avatarSource={require("./images/avatar.png")} 
+              actionSource={require("./images/comment.png")} 
+              users="Zach" 
+              action="commented"
+            />}
+            {(activeTab === 'Mentions' || activeTab === 'All') && <Mention 
+              link="/social_feed" 
+              avatarSource={require("./images/avatar.png")} 
+              actionSource={require("./images/mad.png")} 
+              users="AAAAA" 
+              action="reacted"
+            />}
+          {(activeTab === 'Alerts' || activeTab === 'All') && <Warning
+            link="/safety_map"
+            action="warn"
+            location="[location]"
+            user="Shawn"
+          />}
+          {(activeTab === 'Events' || activeTab === 'All') && <Reminder
+            link="/event"
+            name="[eventName]"
+            timeLeft="[timeLeft]"
+          />}
+          {(activeTab === 'Mentions' || activeTab === 'All') && <Mention 
+            link="/social_feed" 
+            avatarSource={require("./images/avatar.png")} 
+            actionSource={require("./images/heart.png")} 
+            users="Gabe" 
+            action="reacted"
+          />}
+          {(activeTab === 'Mentions' || activeTab === 'All') && <Mention 
+            link="/social_feed" 
+            avatarSource={require("./images/avatar.png")} 
+            actionSource={require("./images/comment.png")} 
+            users="HelloWorld" 
+            action="commented"
+          />}
+          {(activeTab === 'Events' || activeTab === 'All') && <Event
+            link="/event"
+            name="[eventName]"
+            organization="@org"
+          />}
+        </ScrollView>               
       </View> 
     </>
   );
 }
+
+//User object should be made
