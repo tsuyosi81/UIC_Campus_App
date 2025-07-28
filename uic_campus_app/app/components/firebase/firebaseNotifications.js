fdb50b27c8ce584a640862b298c3dae112a849e0
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-07-28 13:36:03 -0500

    Set up firebase functionality for notifications

---
+++ uic_campus_app/app/components/firebase/firebaseNotifications.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV8nAAJqFVtxAZMvPTS7aXli3-XE7SNtE",
+  authDomain: "uic-campus-app.firebaseapp.com",
+  databaseURL: "https://uic-campus-app-default-rtdb.firebaseio.com",
+  projectId: "uic-campus-app",
+  storageBucket: "uic-campus-app.firebasestorage.app",
+  messagingSenderId: "334819310965",
+  appId: "1:334819310965:web:bd8b26cdf1a52e9180273c",
+  measurementId: "G-E939RF26YH"
+};
+
+const app = initializeApp(firebaseConfig);
+
