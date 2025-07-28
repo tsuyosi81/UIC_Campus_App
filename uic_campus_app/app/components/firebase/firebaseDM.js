d5b99e554a3858bbf4dc25625f834bf1544ffbb9
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-07-28 15:05:08 -0500

    Started on firebase set up for dm

---
+++ uic_campus_app/app/components/firebase/firebaseDM.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV8nAAJqFVtxAZMvPTS7aXli3-XE7SNtE",
+  authDomain: "uic-campus-app.firebaseapp.com",
+  databaseURL: "https://uic-campus-app-default-rtdb.firebaseio.com",
+  projectId: "uic-campus-app",
+  storageBucket: "uic-campus-app.firebasestorage.app",
+  messagingSenderId: "334819310965",
+  appId: "1:334819310965:web:bd8b26cdf1a52e9180273c",
+  measurementId: "G-E939RF26YH",
+  databaseURL: "https://uic-campus-app-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
