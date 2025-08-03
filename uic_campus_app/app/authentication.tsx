5b75cb0b65657628e27d4f8c5b7e9487345c8942
Kevin Marroquin <kmarro4@uic.edu>
2025-08-03 12:56:18 -0500

    authentication

--- uic_campus_app/app/authentication.tsx
+++ uic_campus_app/app/authentication.tsx
@@ -1,20 +1,69 @@
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-import { Text, View } from "react-native";
-
-export default function Authentication() {
+const LoginScreen: React.FC = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>This is the authentication.tsx
+    <View style={styles.container}>
+      <Text style={styles.title}>Enter your school email</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+
+      <Text style={styles.tosPrompt}>
+        By logging on, you agree to the
       </Text>
-      <Text>
-        made by justin
+      <Text style={styles.tosPrompt}>
+        <Text style={styles.link}> Terms of Service </Text> 
+        and 
+        <Text style={styles.link}> Privacy Policy</Text>
       </Text>
+
+      <View style={styles.button}>
+        <Button title="Continue" onPress={() => {}} />
+          
+      </View>
     </View>
   );
-}
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 32,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 14,
+    borderRadius: 8,
+    marginBottom: 16,
+    fontSize: 16,
+  },
+  button: {
+    marginTop: 8,
+    marginBottom: 24,
+  },
+  tosPrompt: {
+    textAlign: 'center',
+    fontSize: 14,
+    color: '#777',
+  },
+  link: {
+    color: '#007bff',
+    fontWeight: '500',
+  },
+});
